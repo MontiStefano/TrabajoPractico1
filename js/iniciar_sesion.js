e27ab4e8634f6de6c3a43ff3bbdb98745f8b0a2d
MontiStefano <monti.stefano1324@gmail.com>
2025-09-08 18:52:13 -0300

    inicio de sesion y registro funcionando

--- js/iniciar_sesion.js
+++ js/iniciar_sesion.js
@@ -14,20 +14,32 @@ btnIniciar.addEventListener('click', () => {
 
 const formularioInicio = document.getElementById("formularioInicio");
 
-formularioInicio.addEventListener("submit", function (e) {
+formularioInicio.addEventListener("submit", async function (e) {
   e.preventDefault();
-  if (validarLogin()) {
-    alert("Sesión iniciada con éxito");
-  } else {
-    alert("Datos erróneos");
-  }
-});
 
-function validarLogin() {
   const usuario = document.getElementById("userInicio").value.trim();
-  const contrasenia = document.getElementById("passInicio").value.trim();
+  const contra = document.getElementById("passInicio").value.trim();
 
-  return usuario !== "" && contrasenia !== "";
+  const response = await fetch(`http://localhost:3000/usuarios?usuario=${usuario}`);
+  const usuarios = await response.json();
+
+
+    if(usuarios.length > 0 && usuarios[0].contra === contra){     // si hay un usuario y la contraseña ingresada coincide
+      alert("Usuario logeado con exito")
+      window.location.href = `../index.html`;
+    }else{
+      alert("Usuario o contraseña incorrectos")
+    }
+  });
+
+
+function validarLogin(usuario, contra) {
+  if(usuario === "" || contra === ""){
+    alert("Por favor, complete el formulario");
+    return false;
+  }else {
+    return true;
+  }
 }
 
 
@@ -35,27 +47,28 @@ function validarLogin() {
 
 const formularioRegistro = document.getElementById("formularioRegistro");
 
-formularioRegistro.addEventListener("submit", function (e) {
+formularioRegistro.addEventListener("submit", async function (e) {
   e.preventDefault();
 
   const usuario = document.getElementById("userRegistro").value.trim();
   const email = document.getElementById("emailRegistro").value.trim();
   const contra = document.getElementById("passRegistro").value.trim();
 
-  if (validarRegistro(usuario, email, contra)) {
-    // si el registro es valido, realiza el fetch
+  if (validarRegistro(usuario, email, contra) && await usuarioExistente(usuario)) {    // si el formulario es valido y si no hay un usuario ya existente  
+
     fetch("http://localhost:3000/usuarios", {                   // fetch a usuarios
       method: "POST",                                           // metodo POST para enviar el formulario a la db.json
       headers: { "Content-Type": "application/json" },          // indica que es formato JSON
       body: JSON.stringify({ usuario, email, contra })         // contenido del JSON
     })
-      .then(res => res.json())
-      .then(data => {
-        console.log("Usuario registrado:", data);           // muestra en la consola la informacion del usuario registrado
-        alert("Registrado con exito!");                     // muestra un mensaje al usuario
-        contenedor.classList.remove('activo');              // vuelve a la parte de incio de sesion
-      })
-      .catch(err => console.error(err));
+    .then(res => res.json())
+    .then(data => {
+      console.log("Usuario registrado:", data);           // muestra en la consola la informacion del usuario registrado
+      alert("Registrado con exito!");                     // muestra un mensaje al usuario
+      contenedor.classList.remove('activo');              // vuelve a la parte de incio de sesion
+    })
+    .catch(err => console.error(err));
+    
   }
 });
 
@@ -76,3 +89,16 @@ function validarRegistro(usuario, email, contra) {
 }
 
 
+// devuelve false si ya hay un usuario y true si esta disponible
+async function usuarioExistente(usuario){
+  const response = await fetch(`http://localhost:3000/usuarios?usuario=${usuario}`);
+  const usuarios = await response.json();
+  
+  if (usuarios.length > 0) {
+    // Ya existe un usuario con ese email
+    alert("El usuario ya está registrado");
+    return false;
+  } else {
+    return true;
+  }
+}
